2a5a3f3c193404f74a29d28fdc70c0765423f958
Anderson Manzini <andersonmanzini@gmail.com>
2018-01-14 18:09:09 -0200

    Novos arquivos

--- exercicios/helloworld.php
+++ exercicios/helloworld.php
@@ -8,8 +8,19 @@
 
 <body>
 	<h1>Curso de PHP Completo - HCODE</h1>
+
+<?php
+
+	echo "Hello World!";
+	echo "<br><br>";
+	echo "Olá Mundo!";
+	
+?>
+
 	<ul>
-		<li><a href="exercicios/helloworld.php">Hello World</a></li>
+		<li><a href="../index.php">Voltar</a></li>
 	</ul>
 </body>
 </html>
+
+
